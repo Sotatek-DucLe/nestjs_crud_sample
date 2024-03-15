d724731b7b3ee59d07bce261823d42a318d3017c
Sotatek-DucLe <duc.le@sotatek.com>
2024-03-15 15:31:35 +0700

    generate users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 import { TeachersModule } from './teachers/teachers.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         StudentsModule,
         TeachersModule,
+        UsersModule,
     ],
     controllers: [],
     providers: [AppService],
